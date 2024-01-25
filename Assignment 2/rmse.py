9ae084985a17e57ef71074e9bdefec4cd3bf72d0
JsGit04 <156146864+JsGit04@users.noreply.github.com>
2024-01-24 23:05:53 -0500

    Delete Assignment 2 directory

--- Assignment 2/rmse.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def rmse(predictions, targets):
-    pred = np.array(predictions)
-    tar = np.array(targets)
-    squared_diff = (pred - tar) ** 2
-    mean_squared_diff = np.mean(squared_diff)
-    rmse = np.sqrt(mean_squared_diff)
-    return rmse
